91662a63a69b5aff9db87bf2784ad156bc602df7
NoahBrezet <143404345+NoahBrezet@users.noreply.github.com>
2023-09-05 13:28:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const body = document.body;
 
 toggleModeButton.addEventListener('click', () => {
     if (body.classList.contains('dark-mode')) {
-        // Schakel over naar lichte modus
         body.classList.remove('dark-mode');
     } else {
-        // Schakel over naar donkere modus
         body.classList.add('dark-mode');
     }
 });
