074f3ef1f6514d786ddcaa69abd4422b79acbb13
NoahBrezet <143404345+NoahBrezet@users.noreply.github.com>
2023-09-15 11:12:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const toggleModeButton = document.getElementById('toggleModeButton');
-const body = document.body;
 
-toggleModeButton.addEventListener('click', () => {
-    if (body.classList.contains('dark-mode')) {
-        body.classList.remove('dark-mode');
-    } else {
-        body.classList.add('dark-mode');
-    }
-});
